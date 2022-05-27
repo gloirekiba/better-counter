8af488ce8f0394eb4b712a3de26d1a5ec7f8628e
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 01:09:18 +0200

    ✨ add set name & add counter state

--- src/components/CounterCreate.jsx
+++ src/components/CounterCreate.jsx
@@ -1,29 +1,38 @@
-const CounterCreate = () => {
+import { useState } from "react";
+
+const CounterCreate = ({ addCounter }) => {
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("white");
+
+  function handleSetname({ target }) {
+    setName(target.value);
+  }
+
+  function handleAddCounter() {
+    if (name.trim() === "") return;
+    addCounter(name, color);
+  }
+
   return (
     <div className="counter-create">
       <h3 className="title">Add Counter</h3>
-      <input type="text" className="counter-create-name" placeholder="Counter name" />
+      <input
+        onChange={handleSetname}
+        value={name}
+        type="text"
+        className="counter-create-name"
+        placeholder="Counter name"
+      />
       <div className="counter-create-colors">
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
-        <span className="counter-create-color"></span>
         <span className="counter-create-color"></span>
         <span className="counter-create-color"></span>
         <span className="counter-create-color"></span>
       </div>
       <div className="counter-create-buttons">
         <button className="counter-create-btn">Cancel</button>
-        <button className="counter-create-btn">Save</button>
+        <button onClick={handleAddCounter} className="counter-create-btn">
+          Save
+        </button>
       </div>
     </div>
   );
