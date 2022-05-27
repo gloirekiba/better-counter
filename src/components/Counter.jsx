484ba403ac00ddfd76fc19fd6712d5c08299a78f
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 01:11:23 +0200

    ✨ add handle count feature

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,30 @@
-const Counter = () => {
+const COUNTER_INCREASE = 1;
+const COUNTER_DECREASE = COUNTER_INCREASE * -1;
+
+const Counter = ({ counter, setCount }) => {
+  const canDecrease = counter.count === 0;
+
+  function handleCount(value) {
+    setCount(counter.id, value);
+  }
+
   return (
     <div className="counter">
-      <button className="btn decrease">-</button>
+      <button
+        onClick={() => handleCount(COUNTER_DECREASE)}
+        disabled={canDecrease}
+        className="btn decrease"
+      >
+        -
+      </button>
       <div className="counter-data">
-        <span className="counter-name">eat banana</span>
-        <span className="counter-count">0</span>
-        <span className="counter-last-count">never</span>
+        <span className="counter-name">{counter.name}</span>
+        <span className="counter-count">{counter.count}</span>
+        <span className="counter-last-update">{counter.lastUpdate}</span>
       </div>
-      <button className="btn increase">+</button>
+      <button onClick={() => handleCount(COUNTER_INCREASE)} className="btn increase">
+        +
+      </button>
     </div>
   );
 };
