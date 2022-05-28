b1bd138fbf1d0ddafaed72389cd65b0fb20e45f4
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 20:31:00 +0200

    🗃️ update colors database

--- src/data/ColorList.js
+++ src/data/ColorList.js
@@ -1,3 +1,27 @@
-const COLORS = ["white", "aqua", "azure", "beige", "rebeccapurple", "tomato"];
+const COLORS = [
+  "white",
+  "aqua",
+  "azure",
+  "beige",
+  "rebeccapurple",
+  "tomato",
+  "aliceblue",
+  "antiquewhite",
+  "aquamarine",
+  "bisque",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "tan",
+  "cornsilk",
+  "yellowgreen",
+];
 
 export default COLORS;
