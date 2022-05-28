611764f025cfa211e5978df1f4ae9b41363a9af4
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 04:30:31 +0200

    💄 add transparent overlay

--- src/components/CreateCounter/index.jsx
+++ src/components/CreateCounter/index.jsx
@@ -17,21 +17,32 @@ const CreateCounter = ({ addCounter }) => {
   }
 
   return (
-    <Container>
-      <Title>Add Counter</Title>
-      <Input onChange={handleSetName} value={name} type="text" placeholder="Counter name" />
-      <ColorsList />
-      <ButtonsContainer>
-        <Button>Cancel</Button>
-        <Button onClick={handleAddCounter}>Save</Button>
-      </ButtonsContainer>
-    </Container>
+    <Overlay>
+      <Container>
+        <Title>Add Counter</Title>
+        <Input onChange={handleSetName} value={name} type="text" placeholder="Counter name" />
+        <ColorsList />
+        <ButtonsContainer>
+          <Button>Cancel</Button>
+          <Button onClick={handleAddCounter}>Save</Button>
+        </ButtonsContainer>
+      </Container>
+    </Overlay>
   );
 };
 
+const Overlay = styled.div`
+  position: fixed;
+  z-index: 3;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
 const Container = styled.div`
   position: fixed;
-  z-index: 2;
   top: 20%;
   left: 50%;
   transform: translate(-50%, -20%);
@@ -43,6 +54,7 @@ const Container = styled.div`
   padding: 2em;
   border-radius: 5px;
   background-color: rgb(181, 177, 177);
+  background-color: #fff;
   box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.144);
 `;
 
@@ -56,7 +68,8 @@ const Input = styled.input`
   width: 100%;
   font-size: 2.3rem;
   border: none;
-  border-bottom: 2px solid #fff;
+  padding: 0.3em 0;
+  border-bottom: 2px solid #4444446a;
   outline: none;
   background-color: transparent;
 `;
