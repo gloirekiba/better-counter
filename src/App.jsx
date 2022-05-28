3f5f3f03b11e45688c3a4c26a28d3cedee31660e
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 06:15:56 +0200

    💄 update ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import CreateCounter from "./components/CreateCounter";
-import AddCounter from "./components/AddCounter";
 import CounterList from "./components/Counter";
 
 const App = () => {
   const [counters, setCounters] = useState([]);
+  const [display, setDisplay] = useState(false);
 
   function addCounter(name, color) {
     setCounters([
@@ -26,9 +26,16 @@ const App = () => {
 
   return (
     <Container>
-      <CreateCounter addCounter={addCounter} />
-      <CounterList counters={counters} setCount={setCount} />
-      <AddCounter />
+      {display && <CreateCounter setDisplay={setDisplay} addCounter={addCounter} />}
+      {counters.length === 0 ? (
+        <NoCounterFound>
+          No counter have been found, please create some by cliking to the <Strong>(&#43;)</Strong>{" "}
+          button bellow
+        </NoCounterFound>
+      ) : (
+        <CounterList counters={counters} setCount={setCount} />
+      )}
+      {!display && <Show onClick={() => setDisplay(!display)}>&#43;</Show>}
     </Container>
   );
 };
@@ -37,6 +44,30 @@ const Container = styled.div`
   width: 95%;
   max-width: 1200px;
   margin: 0 auto;
+`;
+
+const NoCounterFound = styled.h3`
+  font-size: 4em;
+  font-weight: 400;
+  text-align: center;
+`;
+
+const Strong = styled.strong`
+  font-weight: 700;
+`;
+
+const Show = styled.button`
+  position: fixed;
+  right: 5%;
+  bottom: 10%;
+  width: 50px;
+  height: 50px;
+  padding: 0.5em;
+  font-size: 2rem;
+  font-weight: 700;
+  background-color: #fff;
+  border-radius: 50%;
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.144);
 `;
 
 export default App;
