202b6536dca154482c940f70ba6ef4248a139a14
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 03:54:30 +0200

    ♻️ refactor code & add styled component

--- src/components/ColorSelector/index.jsx
+++ src/components/ColorSelector/index.jsx
@@ -1,19 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
 import COLORS from "../../data/ColorList";
 
-const Colors = () => {
+const ColorsList = () => {
   return (
-    <>
+    <Container>
       {COLORS.map((color, index) => (
-        <button
-          key={index}
-          style={{ backgroundColor: color }}
-          title={color}
-          className="counter-create-color"
-          value={color}
-        ></button>
+        <Button key={index} style={{ backgroundColor: color }} title={color} value={color} />
       ))}
-    </>
+    </Container>
   );
 };
 
-export default Colors;
+const Container = styled.div`
+  margin-top: 1em;
+  padding: 1em 1em 1em 0;
+  display: flex;
+  gap: 0.4em;
+  overflow-x: scroll;
+`;
+
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  flex-shrink: 0;
+  cursor: pointer;
+`;
+
+export default ColorsList;
