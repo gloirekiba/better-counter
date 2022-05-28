23e56475da040fb0a24e3d99adf975990138453e
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 18:38:53 +0200

    ✨ add edit & delete counter features

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,31 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
 
-import CreateCounter from "./components/CreateCounter";
-import CounterList from "./components/Counter";
-import { flushSync } from "react-dom/cjs/react-dom.production.min";
+import ManageCounter from "./components/ManageCounter";
+import CounterList from "./components/CounterList";
 
 const App = () => {
   const [counters, setCounters] = useState([]);
-  const [display, setDisplay] = useState(false);
+  const [showCounterCreate, setShowCounterCreate] = useState(false);
 
   function addCounter(name, color) {
     setCounters([...counters, { id: uuidv4(), name, count: 0, color: color, lastUpdate: null }]);
   }
 
-  function editCounter(id, data) {}
+  function deleteCounter(id) {
+    const newCounters = [...counters];
+    newCounters.forEach((counter, index) => counter.id === id && newCounters.splice(index, 1));
+    setCounters(newCounters);
+  }
 
-  function setCount(id, value) {
+  function editCounter(id, data) {
+    const newCounters = [...counters];
+    let counter = newCounters.find((counter) => counter.id === id);
+    counter = Object.assign(counter, data);
+    setCounters(newCounters);
+  }
+
+  function updateCount(id, value) {
     const newCounters = [...counters];
     const counter = newCounters.find((counter) => counter.id === id);
     counter.count += value;
@@ -26,18 +36,33 @@ const App = () => {
   }
 
   return (
-    <Container>
-      {display && <CreateCounter setDisplay={setDisplay} addCounter={addCounter} />}
-      {counters.length === 0 ? (
-        <NoCounterFound>
-          No counter have been found, please create some by cliking on the <Strong>(&#43;)</Strong>{" "}
-          button bellow
-        </NoCounterFound>
-      ) : (
-        <CounterList counters={counters} setCount={setCount} />
+    <>
+      {showCounterCreate && (
+        <ManageCounter
+          context="create"
+          addCounter={addCounter}
+          setShowCounterCreate={setShowCounterCreate}
+        />
       )}
-      {!display && <Show onClick={() => setDisplay(!display)}>&#43;</Show>}
-    </Container>
+      <Container>
+        {counters.length === 0 ? (
+          <NoCounterFound>
+            No counter have been found, please create some by cliking on the{" "}
+            <Strong>(&#43;)</Strong> button bellow
+          </NoCounterFound>
+        ) : (
+          <CounterList
+            counters={counters}
+            updateCount={updateCount}
+            editCounter={editCounter}
+            deleteCounter={deleteCounter}
+          />
+        )}
+        {!showCounterCreate && (
+          <ShowManager onClick={() => setShowCounterCreate(true)}>&#43;</ShowManager>
+        )}
+      </Container>
+    </>
   );
 };
 
@@ -57,7 +82,7 @@ const Strong = styled.strong`
   font-weight: 700;
 `;
 
-const Show = styled.button`
+const ShowManager = styled.button`
   position: fixed;
   right: 5%;
   bottom: 10%;
