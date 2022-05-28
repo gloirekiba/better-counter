9a78365f7ff994ef3a3e521dbbf7552e94a0e8c4
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 03:54:37 +0200

    ♻️ refactor code & add styled component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import styled from "styled-components";
+
 const COUNTER_INCREASE = 1;
 const COUNTER_DECREASE = COUNTER_INCREASE * -1;
 
@@ -9,24 +12,53 @@ const Counter = ({ counter, setCount }) => {
   }
 
   return (
-    <div className="counter">
-      <button
-        onClick={() => handleCount(COUNTER_DECREASE)}
-        disabled={canDecrease}
-        className="btn decrease"
-      >
+    <Container>
+      <Button onClick={() => handleCount(COUNTER_DECREASE)} disabled={canDecrease}>
         -
-      </button>
-      <div className="counter-data">
-        <span className="counter-name">{counter.name}</span>
-        <span className="counter-count">{counter.count}</span>
-        <span className="counter-last-update">{counter.lastUpdate}</span>
-      </div>
-      <button onClick={() => handleCount(COUNTER_INCREASE)} className="btn increase">
-        +
-      </button>
-    </div>
+      </Button>
+      <Datas>
+        <Name>{counter.name}</Name>
+        <Count>{counter.count}</Count>
+        <LastUpdate>{counter.lastUpdate}</LastUpdate>
+      </Datas>
+      <Button onClick={() => handleCount(COUNTER_INCREASE)}>+</Button>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #fff;
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.144);
+`;
+
+const Button = styled.button`
+  padding: 0.1em 0.6em;
+  font-size: 5rem;
+  font-weight: 400;
+  cursor: pointer;
+`;
+
+const Datas = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1em;
+  text-align: center;
+`;
+
+const Name = styled.span`
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+const Count = styled.span`
+  font-size: 3em;
+`;
+const LastUpdate = styled.span`
+  font-size: 1.6rem;
+`;
 
 export default Counter;
