27c2b01dbab1ec9a761e39f89fad2fe79ff4a122
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 03:54:49 +0200

    ♻️ refactor code & add styled component

--- src/components/CreateCounter/index.jsx
+++ src/components/CreateCounter/index.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Colors from "../ColorSelector";
+import ColorsList from "../ColorSelector";
 
-const CounterCreate = ({ addCounter }) => {
+const CreateCounter = ({ addCounter }) => {
   const [name, setName] = useState("");
   const [color, setColor] = useState("white");
 
@@ -16,26 +17,60 @@ const CounterCreate = ({ addCounter }) => {
   }
 
   return (
-    <div className="counter-create">
-      <h3 className="title">Add Counter</h3>
-      <input
-        onChange={handleSetName}
-        value={name}
-        type="text"
-        className="counter-create-name"
-        placeholder="Counter name"
-      />
-      <div className="counter-create-colors">
-        <Colors />
-      </div>
-      <div className="counter-create-buttons">
-        <button className="counter-create-btn">Cancel</button>
-        <button onClick={handleAddCounter} className="counter-create-btn">
-          Save
-        </button>
-      </div>
-    </div>
+    <Container>
+      <Title>Add Counter</Title>
+      <Input onChange={handleSetName} value={name} type="text" placeholder="Counter name" />
+      <ColorsList />
+      <ButtonsContainer>
+        <Button>Cancel</Button>
+        <Button onClick={handleAddCounter}>Save</Button>
+      </ButtonsContainer>
+    </Container>
   );
 };
 
-export default CounterCreate;
+const Container = styled.div`
+  position: fixed;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -20%);
+  width: 95%;
+  max-width: 600px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+  border-radius: 5px;
+  background-color: rgb(181, 177, 177);
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.144);
+`;
+
+const Title = styled.h3`
+  font-size: 2.5rem;
+  margin-bottom: 1em;
+  text-transform: capitalize;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: 2.3rem;
+  border: none;
+  border-bottom: 2px solid #fff;
+  outline: none;
+  background-color: transparent;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1em;
+  margin-top: auto;
+`;
+
+const Button = styled.button`
+  font-size: 1.6rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+export default CreateCounter;
