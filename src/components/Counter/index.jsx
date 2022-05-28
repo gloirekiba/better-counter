7ac632c9faaf0feafec58874d37582b995857700
Gloire Kiba <kibagloire1530@gmail.com>
2022-05-28 03:54:44 +0200

    ♻️ refactor code & add styled component

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,13 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
 import Counter from "./Counter";
 
 const CounterList = ({ counters, setCount }) => {
   return (
-    <div className="counter-list">
+    <Container>
       {counters.map((counter) => (
         <Counter key={counter.id} counter={counter} setCount={setCount} />
       ))}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
 
 export default CounterList;
