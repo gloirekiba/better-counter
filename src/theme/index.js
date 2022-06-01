ae08c7c58297310bedcc0c0ff41c8514ae3c99c0
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-01 14:15:11 +0200

    💄 add theme colors

---
+++ src/theme/index.js
@@ -0,0 +1,33 @@
+const theme = {
+  colors: {
+    primary: "red",
+    secondary: "green",
+    extras: [
+      "white",
+      "aqua",
+      "azure",
+      "beige",
+      "rebeccapurple",
+      "tomato",
+      "aliceblue",
+      "antiquewhite",
+      "aquamarine",
+      "bisque",
+      "blanchedalmond",
+      "blue",
+      "blueviolet",
+      "brown",
+      "burlywood",
+      "cadetblue",
+      "chartreuse",
+      "chocolate",
+      "coral",
+      "cornflowerblue",
+      "tan",
+      "cornsilk",
+      "yellowgreen",
+    ],
+  },
+};
+
+export default theme;
