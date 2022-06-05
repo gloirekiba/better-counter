674bd0c047d737a535f6a26c5fca8f2da4754cfa
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-05 21:23:36 +0200

    🚧  change *.js file to *.ts *.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import { createRoot } from "react-dom/client";
 import GlobalStyle from "./style.global";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLDivElement);
+
 root.render(
   <StrictMode>
     <GlobalStyle />
